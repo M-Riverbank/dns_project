5cc394998e9ab0972b11b3c1a10b20f56b91d157
笔记本 <3388047303@qq.com>
2024-01-11 11:55:29 +0800

    代码调整

---
+++ src/main/scala/dsy/model/profileTag/export/export_data.scala
@@ -0,0 +1,24 @@
+package dsy.model.profileTag.`export`
+
+import dsy.model.AbstractModel
+import org.apache.spark.sql.DataFrame
+
+class export_data extends AbstractModel("导出Hbase用户画像数据"){
+  /**
+   * 抽象方法，对数据的具体处理,由实现类完善
+   *
+   * @param businessDF 业务数据
+   * @param mysqlDF    后端数据
+   * @return 处理后的数据
+   */
+  override def handle(businessDF: DataFrame, mysqlDF: DataFrame): DataFrame = {
+    businessDF.show(100,truncate = false)
+    null
+  }
+}
+
+object export_data{
+  def main(args: Array[String]): Unit = {
+    new export_data().execute(37)
+  }
+}
